99b73741ae8dec53789c0a911f8cbea19ec1ee0d
Younes Amajid <yamajid@e3r5p4.1337.m>
2025-08-27 01:16:48 +0100

    deploy backend on a container

---
+++ backend/core/script.sh
@@ -0,0 +1,9 @@
+
+
+pip install --no-cache-dir -r requirements.txt
+
+python manage.py makemigrations chat_backend authentication
+python manage.py migrate
+
+
+python -u manage.py runserver 0.0.0.0:8000 
