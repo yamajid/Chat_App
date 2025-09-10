eaf3ea2d6f63881c152cc5df22a2aecd39a71f8f
yamajid <yamajid@student.1337.ma>
2025-09-10 23:24:28 +0100

    update ngnix to work without https

--- deployment/nginx/script.sh
+++ deployment/nginx/script.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-mkdir -p /etc/nginx/ssl
+# mkdir -p /etc/nginx/ssl
 
-apk add --no-cache openssl
+# apk add --no-cache openssl
 
-if [ ! -f /etc/nginx/ssl/mycert.crt ]; then
-  echo "Generating self-signed SSL cert..."
-  openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/nginx/ssl/mykey.key \
-    -out /etc/nginx/ssl/mycert.crt \
-    -subj "/CN=localhost"
-fi
+# if [ ! -f /etc/nginx/ssl/mycert.crt ]; then
+#   echo "Generating self-signed SSL cert..."
+#   openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+#     -keyout /etc/nginx/ssl/mykey.key \
+#     -out /etc/nginx/ssl/mycert.crt \
+#     -subj "/CN=localhost"
+# fi
 
 echo "Starting Nginx..."
 nginx -g "daemon off;"
